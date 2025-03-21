2e801f3589be1eaf01831c704aa71b53940cae12
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-03-21 05:55:07 +0200

    Edit CharactersCubit & CharactersState

--- lib/busniess_logic/cubit/characters_cubit.dart
+++ lib/busniess_logic/cubit/characters_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
+import 'package:characters_app/data/models/character.dart';
+import 'package:characters_app/data/repos/character_repo.dart';
+import 'package:flutter/material.dart';
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  CharactersCubit(this.characterRepo) : super(CharactersInitial());
+
+  final CharacterRepo characterRepo;
+
+  Future<void> getAllCharacter() async {
+    emit(CharactersLoading()); 
+    try {
+      final characters = await characterRepo.getAllCharacters();
+      emit(CharactersSuccess(characters: characters)); 
+    } catch (e) {
+      emit(CharactersFailure(errorMessage: e.toString())); 
+    }
+  }
 }
--- lib/busniess_logic/cubit/characters_state.dart
+++ lib/busniess_logic/cubit/characters_state.dart
@@ -4,3 +4,20 @@ part of 'characters_cubit.dart';
 sealed class CharactersState {}
 
 final class CharactersInitial extends CharactersState {}
+
+final class CharactersLoading extends CharactersState {}
+
+final class CharactersSuccess extends CharactersState {
+  final List<Character> characters;
+
+  CharactersSuccess({required this.characters});
+}
+
+final class CharactersFailure extends CharactersState {
+  final String errorMessage;
+
+  CharactersFailure({required this.errorMessage});
+
+  @override
+  String toString() => 'CharactersFailure: $errorMessage';
+}
