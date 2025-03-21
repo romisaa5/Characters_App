64cc5a6646dcbb5ab76aa20719a6c5bf6d1c677a
Romisaa Fadel <163692583+romisaa5@users.noreply.github.com>
2025-03-21 06:33:40 +0200

    add  BlocProvider to home screen at approute

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,15 +1,38 @@
+import 'package:characters_app/busniess_logic/cubit/characters_cubit.dart';
 import 'package:characters_app/constants/string.dart';
+import 'package:characters_app/data/Api_services/characters_api_service.dart';
+import 'package:characters_app/data/repos/character_repo.dart';
 import 'package:characters_app/presentation/screens/character_details_screen.dart';
 import 'package:characters_app/presentation/screens/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
-  Route ? generateRoute(RouteSettings setting) {
+  late CharacterRepo characterRepo;
+  late CharactersCubit charactersCubit;
+  AppRouter() {
+    characterRepo = CharacterRepo(CharactersApiService());
+    charactersCubit = CharactersCubit(characterRepo);
+  }
+
+  Route? generateRoute(RouteSettings setting) {
     switch (setting.name) {
       case characterScreen:
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider(
+                create:
+                    (BuildContext context) => CharactersCubit(characterRepo),
+                child: HomeScreen(),
+              ),
+        );
       case characterDetailsScreen:
         return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
+      default:
+        return MaterialPageRoute(
+          builder:
+              (_) => Scaffold(body: Center(child: Text('Page Not Found!'))),
+        );
     }
   }
 }
